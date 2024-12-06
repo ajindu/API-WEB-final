8d1ec0b2736fed48a6070ab90ae16c838c12245e
Sang Aji Indutoro <aji.indu06@gmail.com>
2024-12-06 17:43:17 +0700

    Update WebPageInput.java

--- src/test/java/pages/WebPageInput.java
+++ src/test/java/pages/WebPageInput.java
@@ -16,8 +16,8 @@ public class WebPageInput {
 
     public void inputName(String name){
        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-       wait.until(ExpectedConditions.elementToBeClickable(input_name));
-       input_name.sendKeys(name);
+       wait.until(ExpectedConditions.visibilityOfElementLocated(input_name);
+       driver.findElement(input_name).sendKeys(name);
     }
     public void inputCountry(String country){
         driver.findElement(input_country).sendKeys(country);
